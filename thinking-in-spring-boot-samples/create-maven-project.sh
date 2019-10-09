da43ec78930173488e4dbb755c937767121a4c6f
kai.tian <kai.tian@ngaa.com.cn>
2019-10-10 00:55:17 +0800

    use mvn build spring-boot app

---
+++ thinking-in-spring-boot-samples/create-maven-project.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+ mvn archetype:generate -DgroupId=thinking-in-spring-boot-samples       \
+ -DartifactId=first-spring-boot-application                             \
+ -Dversion=1.0.0-SNAPSHOT   \
+ -DinteractiveMode=false    \
+ -Dpackage=thinking.in.spring.boot
